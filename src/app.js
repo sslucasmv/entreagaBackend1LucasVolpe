5a6d3d7759698bc7e2dd9b5aa5c5f5ec1e4577e1
sslucasmv <lmvolpe24@gmail.com>
2024-07-29 17:20:57 -0300

    archivos subidos 1ra entrega backend

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express();
+const PUERTO = 8080;
+const productRouter = require("./routes/products.router.js")
+const cartRouter = require("./routes/carts.router.js")
+
+
+// middelware
+app.use(express.json());
+// le aviso al server que voy a usar formato  json
+
+app.use("/api/products",productRouter);
+app.use("/api/carts",cartRouter);
+
+
+
+
+
+app.listen(PUERTO, () => {
+  console.log(`Servidor escuchando en el puerto ${PUERTO}`);
+});
+ 
